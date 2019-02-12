77716a8efa166e75fdc0d3b9542e9f238d5ce048
Avi7997 <avipatel7997@gmail.com>
2019-02-12 15:41:36 -0500

    CF 538 D problem added

---
+++ Random Practice/CF_538_D.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+#define EPS 1e-14
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define INFLL (ll)1e18 //Long long infinity
+#define INF (int)1e9 //Int infinity
+#define MX 100005
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<vd> vvd;
+typedef vector<string> vstr;
+
+#define us unordered_set
+#define um unordered_map
+#define bs bitset
+
+typedef vector<um<int, int>> graph;
+
+#define RANGE(i,a,b,d) for (int i=min((int)a,(int)b); i<max((int)a,(int)b); i+=d)
+#define RRANGE(i,a,b,d) for (int i=max((int)a,(int)b); i>min((int)a,(int)b); i+=d)
+#define FOR(i,a,b) RANGE(i,a,b,1)
+#define RFOR(i,a,b) RRANGE(i,a,b,-1)
+#define REP(i,s) FOR(i,0,s)
+#define RREP(i,s) RFOR(i,s-1,-1)
+#define FORIT(it,l) for (auto it = l.begin(); it != l.end(); it++)
+#define EACH(x,v) for (auto &x : v)
+
+#define sz(x) (int)(x).size()
+#define len(x) (int)sizeof(x)/sizeof(*x)
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define contains(m,x) (m.find(x) != m.end()) // check if an element in a map
+#define isIn(S, s) (S.find(s) != string::npos) // check if substring
+
+#define pv(v) EACH(x, v) cout << x << " "; cout << endl; // print vector/array
+#define pvv(vv) EACH(xx, vv){pv(xx);} // print 2-d vector/2-d array
+#define pm(m) EACH(x, m) cout << x.F << ":" << x.S << " "; cout << endl; //print map/lookup table
+
+namespace std{
+  // Used for hashing pair
+  template <> struct hash<pi> {
+    inline size_t operator()(const pi &v) const {
+      hash<int> int_hasher;
+      return int_hasher(v.F) ^ int_hasher(v.S);
+    }
+  };
+};
+
+// This is min queue
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+void print(T t){
+  cout << t << endl;
+}
+
+//Python style printing
+template<typename T, typename... Args>
+void print(T t, Args... args){
+  cout << t << " ";
+  print(args...);
+}
+
+int lcs( vector<int> ar, int n )
+{
+   int L[n+1][n+1];
+   int i, j;
+   int ans = 0;
+
+   /* Following steps build L[m+1][n+1] in bottom up fashion. Note
+      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+   for (i=0; i<=n; i++)
+   {
+     for (j=0; j<=n; j++)
+     {
+       if (i == 0 || j == 0)
+         L[i][j] = 0;
+
+       else if (ar[i-1] == ar[n-1-(j-1)])
+         L[i][j] = L[i-1][j-1] + 1;
+
+       else
+         L[i][j] = max(L[i-1][j], L[i][j-1]);
+       if(i+j == n-1){
+           if(ans<L[i][j])
+            ans = L[i][j];
+       }
+     }
+   }
+
+   /* L[m][n] contains length of LCS for X[0..n-1] and Y[0..m-1] */
+   //cout<<ans<<" : ans"<<endl;
+   return ans;
+}
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin>>n;
+  int ar[n];
+  FOR(i,0,n)
+    cin>>ar[i];
+
+    vector<int> v;
+    v.pb(ar[0]);
+    FOR(i,1,n){
+        if(ar[i]!=ar[i-1])
+            v.pb(ar[i]);
+    }
+    int num = v.size();
+    //cout<<num<<endl;
+    if(num == 1)    cout<<"0";
+    else if(num == 2) cout<<"1";
+    else {
+        cout<<num-1-lcs(v,num);
+    }
+
+  return 0;
+}
